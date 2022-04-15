d2f2b2cb3ec209e8ccbb44a8730f96a3cf024146
Szczurowsky <kamilszczurowski@hotmail.com>
2022-04-15 15:46:10 +0200

    Add comments for operations

--- rat-orm-core/src/main/java/operation/OperationRunnable.java
+++ rat-orm-core/src/main/java/operation/OperationRunnable.java
@@ -2,6 +2,9 @@ package operation;
 
 import java.util.HashMap;
 
+/**
+ * Child of runnable, used to run operations.
+ */
 public interface OperationRunnable extends Runnable {
 
     void execute(HashMap<String, Object> options);
